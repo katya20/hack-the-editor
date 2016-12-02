3ef7c928247f233a22157b7cfc115f8c7f380c4e
katya20 <kpanchenko21@gmail.com>
2016-12-02 08:59:21 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var editor = $(parent.document.body)
 editor.find("div").css({"border": "none"});
 var topbar = $(parent.parent.document.body).find("#topbar")
 topbar.find("input").css({background: "lightblue"}, {"color": "white"})
+topbar.find("span").css
 
 //var allDivsInTheEditoritor.find("div")
 
